d6f85532f304135a5e0d2b89322cd6c4d2e2efd9
DIVYA-GULLADURTHI <dprasanna756@gmail.com>
2026-02-09 22:02:02 +0530

    terraform-aws-eks

---
+++ 70-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+} 
+
+variable "environment" {
+    default = "dev"
+} 
+
+variable "zone_id" {
+    default = "Z06262482W32SJ0EI0GN5"
+} 
+
+variable "zone_name" {
+    default = "thulasi.fun"
+}
---
+++ 80-eks/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "divs-remote-state-dev"
+    key    = "roboshop-dev-eks"
+    region = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
